3b8573506882959ccb79dee01066641abd061411
wodev01 <wo.dev01@gmail.com>
2016-04-12 18:35:11 -0700

    Add shop group module.

--- app/scripts/services/shopGroups/groupService.js
+++ app/scripts/services/shopGroups/groupService.js
@@ -1,27 +1,102 @@
 'use strict';
 app.factory('groupService',['$q', 'ErrorMsg',
-  function($q, ErrorMsg) {
-    var groupService = {};
+    function($q, ErrorMsg) {
+        var groupService = {};
 
-    //Get groups for current user partner:
-    groupService.fetchGroups = function(){
-      var defer = $q.defer();
-      CarglyPartner.ajax({
-        url: '/partners/api/groups',
-        type: 'GET',
-        success: function (data) {
-          console.log("Get group data: ",data);
-          defer.resolve(data);
-        },
-        error:function(error) {
-          ErrorMsg.CheckStatusCode(error.status);
-          defer.resolve(error);
-        }
-      });
-      return defer.promise;
-    };
+        //Get groups for current user partner:
+        groupService.fetchGroups = function(){
+            var defer = $q.defer();
+            CarglyPartner.ajax({
+                url: '/partners/api/groups',
+                type: 'GET',
+                success: function (data) {
+                    defer.resolve(data);
+                },
+                error:function(error) {
+                    ErrorMsg.CheckStatusCode(error.status);
+                    defer.resolve(error);
+                }
+            });
+            return defer.promise;
+        };
 
+        //retrieve all the clients for RLO
+        groupService.fetchClients = function(){
+            var defer = $q.defer();
+            CarglyPartner.ajax({
+                url: '/partners/api/agent/clients',
+                type: 'GET',
+                success: function (data) {
+                    defer.resolve(data);
+                },
+                error:function(error) {
+                    ErrorMsg.CheckStatusCode(error.status);
+                    defer.resolve(error);
+                }
+            });
+            return defer.promise;
+        };
 
-    return groupService;
-  }
+        //retrieve the clients using filter for RLO
+        groupService.fetchClientsUsingFilter = function(filter){
+            var defer = $q.defer();
+            CarglyPartner.ajax({
+                url: '/partners/api/agent/clients?filter='+filter,
+                type: 'GET',
+                success: function (data) {
+                    defer.resolve(data);
+                },
+                error:function(error) {
+                  ErrorMsg.CheckStatusCode(error.status);
+                    defer.resolve(error);
+                }
+            });
+            return defer.promise;
+        };
+
+        //Delete a member partner from a group. Requires that current user has access to partner as either an agent or coach.
+        groupService.deleteMemberPartnerFromGroup = function(groupId, partnerId){
+            var defer = $q.defer();
+            CarglyPartner.ajax({
+                url: '/partners/api/groups/'+ groupId +'/members/' + partnerId,
+                type: 'DELETE',
+                success: function (data) {
+                    defer.resolve(data);
+                },
+                error:function(error) {
+                    ErrorMsg.CheckStatusCode(error.status);
+                    defer.resolve(error);
+                }
+            });
+            return defer.promise;
+        };
+
+        // Fetch all members of the group
+        groupService.fetchGroupMembers = function(groupId){
+            var defer = $q.defer();
+            CarglyPartner.ajax({
+                url: '/partners/api/groups/'+ groupId +'/members',
+                type: 'GET',
+                success: function (data) {
+                    defer.resolve(data);
+                },
+                error:function(error) {
+                    ErrorMsg.CheckStatusCode(error.status);
+                    defer.resolve(error);
+                }
+            });
+            return defer.promise;
+        };
+
+        /*-------------- Getter and Setter Method ---------*/
+        var groupObj = {};
+        groupService.setGroupObj = function(newObj){
+          groupObj = newObj;
+        };
+        groupService.getGroupObj = function(){
+          return groupObj;
+        };
+
+        return groupService;
+    }
 ]);
