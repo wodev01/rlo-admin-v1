5b8fc19073a3c44d1b3394f6f4e26bf4c2a4047b
webashlar_developer <sweety.webashlar@gmail.com>
2016-04-13 10:31:54 +0530

    UI changes.

--- app/scripts/controllers/authenticated/settings/usersCtrl.js
+++ app/scripts/controllers/authenticated/settings/usersCtrl.js
@@ -29,10 +29,10 @@ app.controller('usersCtrl',
         });
 
         $scope.userAction = '<div layout="row">' +
-            '<md-button class="md-icon-button md-accent" ng-click="grid.appScope.fnUserEdit(row,$event)">' +
+            '<md-button class="md-icon-button md-primary" ng-click="grid.appScope.fnUserEdit(row,$event)">' +
             '   <md-icon md-font-set="material-icons">edit</md-icon>' +
             '	<md-tooltip md-direction="top">Edit</md-tooltip></md-button>' +
-            '<md-button class="md-icon-button md-warn md-hue-2" ng-click="grid.appScope.fnUserDelete(row,$event)">' +
+            '<md-button class="md-icon-button md-warn" ng-click="grid.appScope.fnUserDelete(row,$event)">' +
             '   <md-icon md-font-set="material-icons">delete</md-icon>' +
             '	<md-tooltip md-direction="top">Delete</md-tooltip>' +
             '</md-button></div>';
